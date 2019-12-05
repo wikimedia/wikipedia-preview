68c8363824b98f657ea59abfaf6813d7567fec5c
Stephane Bisson <sbisson@wikimedia.org>
2019-12-05 15:29:30 -0500

    Polyfill Promise for IE

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,9 @@ const config = {
                 [
                   '@babel/preset-env',
                   {
-                    targets: 'defaults'
+                    targets: 'defaults',
+                    useBuiltIns: 'entry',
+                    corejs: 3
                   }
                 ]
               ]
