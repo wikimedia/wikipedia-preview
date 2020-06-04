757c0f2f72ff8f85f3a3a246309f525f51b144af
Jose Pita <dvpita@gmail.com>
2020-06-04 13:45:10 +0800

    try to run selenium tests

---
+++ test/specs/selenium.js
@@ -0,0 +1,21 @@
+describe('webdriver.io page', () => {
+
+    beforeEach(async ()=>{
+        await browser.url('http://localhost:8080/')
+        const title = await browser.getTitle()
+        await expect(browser).toHaveTitle('Wikipedia Preview demo');
+    })
+
+    it.only('should open article on another tab', async () => {
+        el = await $('div.content-en span:nth-child(1)')
+        await el.click()
+        el=await $('div.wp-text-content > div.wp-title')
+        await el.click()
+        el=await $('.wp-image')
+        await el.click()
+        el=await $('a.wp-link')
+        await el.click()
+        await browser.switchWindow("en.wikipedia.org/wiki/Cat")
+        await expect(browser).toHaveTitle('Cat - Wikipedia');
+    })
+})
