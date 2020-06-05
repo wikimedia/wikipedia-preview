bb2c881c6f06f24577618bd1b5a49636f6603bc1
Eduardo <medina.eag@gmail.com>
2020-06-05 19:12:15 -0400

    Update preview discovery

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { fetchPagePreview } from './api'
 import { createPopup } from './popup'
 import { renderPreview } from './preview'
-import '../style/popup.less'
+import '../style/style.less'
 
 function init({root, selector, lang, popupContainer}={}) {
 	const globalLang = lang || 'en'
