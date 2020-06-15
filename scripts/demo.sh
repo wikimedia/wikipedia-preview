aad097f508e7ac3663a412999ba85c362b51dd98
Stephane Bisson <stephane.c.bisson@gmail.com>
2020-06-15 05:59:47 -0400

    Publish compiled output to gh-pages (#4)
    * Publish compiled output to gh-pages
    
    * Fix config
    
    * Fix config
    
    * Add deploy key
    
    * Just installing gh-pages, no configuration
    
    * gh-pages don't specify version
    
    * install gh-pages locally
    
    * Try a different image
    
    * Add checkout step
    
    * Try the current dir as base
    
    * Deploy the generated demo dir
    
    * Use sed linux arguments
    
    * Don't include the demo dir
    
    * Try to exclude demo dir
    
    * Do we have a double demo?
    
    * Trying to unwrap demo
    
    * Rename steps
    
    * Only update the demo from the master branch
    
    Co-authored-by: Stephane Bisson <sbisson@wikimedia.org>

---
+++ scripts/demo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -rf demo
+mkdir demo
+
+cp index.html demo/
+cp dist/wikipedia-previews.production.js demo/
+sed -i -e 's/dist\/wikipedia-previews\.development\.js/wikipedia-previews\.production\.js/g' demo/index.html
