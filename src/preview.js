d771e6ba04cfa90a4e02a632c2719cce71abc30c
Huei Tan <huei90@gmail.com>
2020-06-23 16:42:12 +0200

    Add gallery placeholder element

--- src/preview.js
+++ src/preview.js
@@ -7,7 +7,12 @@ const renderPreview = (lang, data) => {
 				<div class="wikipediapreviews-header-wordmark">WikipediA</div>
 				<div class="wikipediapreviews-header-closebtn">X</div>
 			</div>
-			<div class="wikipediapreviews-body">${data.extractHtml}</div>
+			<div class="wikipediapreviews-body">
+				${data.extractHtml}
+				<div class="wikipediapreviews-body-gallery">
+					<p> Gallery Placeholder </p>
+				</div>
+			</div>
 			<div class="wikipediapreviews-footer">
 				<span class="wikipediapreviews-footer-cta wikipediapreviews-footer-cta-readmore">Continue Reading</span>
 				<a href="${data.pageUrl}" class="wikipediapreviews-footer-cta wikipediapreviews-footer-cta-readonwiki">Read more on Wikipedia<a>
