08d0e37e67fc966784eff258deaac09cea4881a4
Huei Tan <huei90@gmail.com>
2020-06-30 22:00:04 +0200

    add eslint config file for mocha test by eslint-config-wikimedia

--- test/i18n.test.js
+++ test/i18n.test.js
@@ -1,11 +1,12 @@
-const assert = require('assert')
-const { msg } = require('../src/i18n')
+'use strict';
+const assert = require( 'assert' );
+const { msg } = require( '../src/i18n' );
 
-describe('msg', () => {
-	it('returns the localized key', () => {
-		assert.equal(msg('fr', 'wikipedia'), 'Wikipédia')
-	})
-	it('falls back to english', () => {
-		assert.equal(msg('asdf', 'wikipedia'), 'Wikipedia')
-	})
-})
+describe( 'msg', () => {
+	it( 'returns the localized key', () => {
+		assert.equal( msg( 'fr', 'wikipedia' ), 'Wikipédia' );
+	} );
+	it( 'falls back to english', () => {
+		assert.equal( msg( 'asdf', 'wikipedia' ), 'Wikipedia' );
+	} );
+} );
