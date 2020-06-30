bd4f95eb0f904a78fddf2b34f08074a3bf2208d5
Huei Tan <huei90@gmail.com>
2020-06-30 12:22:13 +0200

    add eslint config file for mocha test by eslint-config-wikimedia

--- src/cachedRequest.js
+++ src/cachedRequest.js
@@ -1,3 +1,4 @@
+'use strict';
 const request = ( url, callback ) => {
 		const xhr = new XMLHttpRequest();
 		xhr.open( 'GET', url );
--- test/api.test.js
+++ test/api.test.js
@@ -1,73 +1,78 @@
-const assert = require('assert')
-const { requestPagePreview } = require('../src/api')
+'use strict';
+const assert = require( 'assert' );
+const { requestPagePreview } = require( '../src/api' ),
 
-const requestMock = (data) => {
-	return (url, transformFn, callback) => {
-		callback(transformFn(data))
-	}
-}
+	requestMock = ( data ) => {
+		return ( url, transformFn, callback ) => {
+			callback( transformFn( data ) );
+		};
+	};
 
-describe('requestPagePreview', () => {
-	it('accepts standard articles only', () => {
-		requestPagePreview('lang', 'title', data => {
-			assert.equal(data, false)
-		}, requestMock({type: 'disambiguation'}))
-	})
+describe( 'requestPagePreview', () => {
+	it( 'accepts standard articles only', () => {
+		requestPagePreview( 'lang', 'title', ( data ) => {
+			assert.equal( data, false );
+		}, requestMock( { type: 'disambiguation' } ) );
+	} );
 
-	it ('accepts ltr articles only', () => {
-		requestPagePreview('lang', 'title', data => {
-			assert.equal(data, false)
-		}, requestMock({type: 'standard', dir: 'rtl'}))
-	})
+	it( 'accepts ltr articles only', () => {
+		requestPagePreview( 'lang', 'title', ( data ) => {
+			assert.equal( data, false );
+		}, requestMock( { type: 'standard', dir: 'rtl' } ) );
+	} );
 
-	it ('transforms the API output', () => {
+	it( 'transforms the API output', () => {
 		const apiOutput = {
-			type: 'standard',
-			dir: 'ltr',
-			displaytitle: 'Dog',
-			extract_html: '<p>A good boy</p>',
-			content_urls: { desktop: {page: 'page-url'}},
-			thumbnail: {source: 'image-url'}
-		}
-		const transformedOutput = {
-			title: 'Dog',
-			extractHtml: '<p>A good boy</p>',
-			pageUrl: 'page-url',
-			imgUrl: 'image-url'
-		}
-		requestPagePreview('lang', 'title', data => {
-			assert.deepEqual(data, transformedOutput)
-		}, requestMock(apiOutput))
-	})
+				type: 'standard',
+				dir: 'ltr',
+				displaytitle: 'Dog',
+				/* eslint-disable camelcase */
+				extract_html: '<p>A good boy</p>',
+				content_urls: { desktop: { page: 'page-url' } },
+				/* eslint-enable camelcase */
+				thumbnail: { source: 'image-url' }
+			},
+			transformedOutput = {
+				title: 'Dog',
+				extractHtml: '<p>A good boy</p>',
+				pageUrl: 'page-url',
+				imgUrl: 'image-url'
+			};
+		requestPagePreview( 'lang', 'title', ( data ) => {
+			assert.deepEqual( data, transformedOutput );
+		}, requestMock( apiOutput ) );
+	} );
 
-	it ('transforms the API output (without image)', () => {
+	it( 'transforms the API output (without image)', () => {
 		const apiOutput = {
-			type: 'standard',
-			dir: 'ltr',
-			displaytitle: 'Dog',
-			extract_html: '<p>A good boy</p>',
-			content_urls: { desktop: {page: 'page-url'}}
-		}
-		const transformedOutput = {
-			title: 'Dog',
-			extractHtml: '<p>A good boy</p>',
-			pageUrl: 'page-url',
-			imgUrl: null
-		}
-		requestPagePreview('lang', 'title', data => {
-			assert.deepEqual(data, transformedOutput)
-		}, requestMock(apiOutput))
-	})
+				type: 'standard',
+				dir: 'ltr',
+				displaytitle: 'Dog',
+				/* eslint-disable camelcase */
+				extract_html: '<p>A good boy</p>',
+				content_urls: { desktop: { page: 'page-url' } }
+				/* eslint-enable camelcase */
+			},
+			transformedOutput = {
+				title: 'Dog',
+				extractHtml: '<p>A good boy</p>',
+				pageUrl: 'page-url',
+				imgUrl: null
+			};
+		requestPagePreview( 'lang', 'title', ( data ) => {
+			assert.deepEqual( data, transformedOutput );
+		}, requestMock( apiOutput ) );
+	} );
 
-	it ('uses the specified language in the URL', () => {
-		requestPagePreview('fr', 'title', () => {}, url => {
-			assert(url.startsWith('https://fr.wikipedia.org/'))
-		})
-	})
+	it( 'uses the specified language in the URL', () => {
+		requestPagePreview( 'fr', 'title', () => {}, ( url ) => {
+			assert( url.startsWith( 'https://fr.wikipedia.org/' ) );
+		} );
+	} );
 
-	it ('encodes the page title in the URL', () => {
-		requestPagePreview('fr', "L'Époque des Châteaux", () => {}, url => {
-			assert(url.endsWith("L'%C3%89poque%20des%20Ch%C3%A2teaux"))
-		})
-	})
-})
+	it( 'encodes the page title in the URL', () => {
+		requestPagePreview( 'fr', "L'Époque des Châteaux", () => {}, ( url ) => {
+			assert( url.endsWith( "L'%C3%89poque%20des%20Ch%C3%A2teaux" ) );
+		} );
+	} );
+} );
