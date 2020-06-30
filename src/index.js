3c332affa1c92563b2a896614cc893818cf2670a
Huei Tan <huei90@gmail.com>
2020-06-30 21:54:50 +0200

    upgrade eslint and use eslint-config-wikimedia

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
-import { requestPagePreview } from './api'
-import { customEvents } from './event'
-import { createPopup } from './popup'
-import { renderPreview } from './preview'
+import { requestPagePreview } from './api';
+import { customEvents } from './event';
+import { createPopup } from './popup';
+import { renderPreview } from './preview';
 
-function init({
-	root = document, 
-	selector = '[data-wikipedia-preview]', 
-	lang = 'en', 
-	popupContainer = document.body }) {
-	const globalLang = lang
-	const popup = createPopup(popupContainer)
-	const events = customEvents(popup)
-	popup.subscribe(events)
+function init( {
+	root = document,
+	selector = '[data-wikipedia-preview]',
+	lang = 'en',
+	popupContainer = document.body } ) {
+	const globalLang = lang,
+		popup = createPopup( popupContainer ),
+		events = customEvents( popup ),
+		showPopup = ( { target } ) => {
+			const title = target.getAttribute( 'data-wp-title' ) || target.textContent,
+				lang = target.getAttribute( 'data-wp-lang' ) || globalLang;
+			requestPagePreview( lang, title, data => {
+				if ( data ) {
+					popup.show( renderPreview( lang, data ), target );
+				}
+			} );
+		};
 
-	const showPopup = ({ target }) => {
-		const title = target.getAttribute('data-wp-title') || target.textContent
-		const lang = target.getAttribute('data-wp-lang') || globalLang
-		requestPagePreview(lang, title, data => {
-			if (data) {
-				popup.show(renderPreview(lang, data), target)
-			}
-		})
-	}
+	popup.subscribe( events );
 
 	Array.prototype.forEach.call(
-		root.querySelectorAll(selector),
+		root.querySelectorAll( selector ),
 		node => {
-			node.addEventListener('mouseenter', showPopup)
+			node.addEventListener( 'mouseenter', showPopup );
 		}
-	)
+	);
 }
 
-export { init }
+export { init };
