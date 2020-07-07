42a726a77a96e546b8d6cae597b44a3c60991ecf
Huei Tan <huei90@gmail.com>
2020-07-07 21:08:43 +0200

    remove unnecessary line

--- test/utils.test.js
+++ test/utils.test.js
@@ -11,7 +11,6 @@ describe( 'getWikipediaAttrFromUrl', () => {
 		{ url: 'https://de.wikipedia.org/w/index.php?title=Katze', expected: { lang: 'de', mobile: false, title: 'Katze' } },
 		{ url: 'https://en.m.wikipedia.org/w/index.php?title=Cat&uselang=de', expected: { lang: 'en', mobile: true, title: 'Cat' } },
 		{ url: 'https://wikimediafoundation.org/', expected: null }
-
 	]
 
 	const testFn = ( test ) => {
