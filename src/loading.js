27e57f6355f68394a308d0651e1c8eb703f0dfa2
Eduardo <medina.eag@gmail.com>
2020-07-29 16:31:02 -0400

    Add loading thumbnail

--- src/loading.js
+++ src/loading.js
@@ -1,12 +1,10 @@
 import '../style/preview.less'
 
 const renderLoading = ( isTouch ) => {
-	console.log( 'loading.js - renderLoading' )
-
 	return `
     <div class="wikipediapreview ${isTouch ? 'mobile' : ''}">
         <div class="wikipediapreview-header">
-          <div class="wikipediapreview-header-image"></div>
+          <div class="wikipediapreview-loading-header-image"></div>
           <div class="wikipediapreview-header-wordmark"></div>
           <div class="wikipediapreview-header-closebtn"></div>
         </div>
