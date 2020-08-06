faa185d9f637d3a4e1acba828683e4747b1cafcb
Stephane Bisson <stephane.c.bisson@gmail.com>
2020-08-06 15:39:52 -0400

    RTL support (#37)
    * RTL support
    
    * Remove hardcoded dir=ltr in popups
    
    * Add additional popup (with gallery) to Hebrew article
    
    * Flip gallery buttons
    
    * Render loading state with the right directionality
    
    Co-authored-by: Stephane Bisson <sbisson@wikimedia.org>

--- src/api.js
+++ src/api.js
@@ -3,14 +3,15 @@ import { cachedRequest } from './cachedRequest'
 const requestPagePreview = ( lang, title, isTouch, callback, request = cachedRequest ) => {
 		const url = `https://${lang}.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent( title )}`
 		request( url, data => {
-			if ( data.type === 'standard' && data.dir === 'ltr' ) {
+			if ( data.type === 'standard' ) {
 				return {
 					title: data.displaytitle,
 					extractHtml: data.extract_html,
 					pageUrl: isTouch ?
 						data.content_urls.mobile.page :
 						data.content_urls.desktop.page,
-					imgUrl: data.thumbnail ? data.thumbnail.source : null
+					imgUrl: data.thumbnail ? data.thumbnail.source : null,
+					dir: data.dir
 				}
 			}
 			return false
