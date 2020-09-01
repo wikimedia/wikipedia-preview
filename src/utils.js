5bdc41ba4323933c8eb555cac629c05cc3a824a7
Eduardo <medina.eag@gmail.com>
2020-09-01 16:15:20 -0400

    Simplify regexList to single reg exp per url format

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,11 @@
 export const getWikipediaAttrFromUrl = url => {
 		const regexList = [
-			// https://he.wikipedia.org/wiki/ירח#Section
-			/^https?:\/\/(\w+)(\.m)?\.wikipedia\.org\/wiki\/([^\u0080-\u00FF]+)\#([^\u0080-\u00FF]+)/,
-			// https://en.wikipedia.org/wiki/Cat?action=edit
-			/^https?:\/\/(\w+)(\.m)?\.wikipedia\.org\/wiki\/([^\u0080-\u00FF]+)\?([^\u0080-\u00FF]+)/,
 			// https://zh.wikipedia.org/wiki/前岐镇"
-			/^https?:\/\/(\w+)(\.m)?\.wikipedia\.org\/wiki\/([^\u0080-\u00FF]+)/,
-			// https://zh.wikipedia.org/w/index.php?title=太阳帆&action=purge
-			// https://en.m.wikipedia.org/w/index.php?title=Cat&uselang=de
-			/^https?:\/\/(\w+)(\.m)?\.wikipedia\.org\/w\/index.php\?title=([^\u0080-\u00FF]+)\&([^\u0080-\u00FF])/,
+			// https://en.wikipedia.org/wiki/Cat#Section
+			/^https?:\/\/(\w+)(\.m)?\.wikipedia\.org\/wiki\/([^#?\u0100-\u017F]+)/,
 			// https://en.wikipedia.org/w/index.php?title=Cat
-			/^https?:\/\/(\w+)(\.m)?\.wikipedia\.org\/w\/index.php\?title=([^\u0080-\u00FF]+)/
+			// https://zh.wikipedia.org/w/index.php?title=太阳帆&action=purge
+			/^https?:\/\/(\w+)(\.m)?\.wikipedia\.org\/w\/index.php\?title=([^&\u0100-\u017F]+)/
 		]
 
 		for ( let i = 0; i < regexList.length; i++ ) {
