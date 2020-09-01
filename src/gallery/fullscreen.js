fea28987878fbcbf33d82f3581e876898e4f233c
Huei Tan <huei90@gmail.com>
2020-09-01 12:18:06 +0200

    Code : Simplify fullscreen component

--- src/gallery/fullscreen.js
+++ src/gallery/fullscreen.js
@@ -1,29 +1,29 @@
 import { renderImageSlider, onShowFn as sliderOnShowFn } from './slider'
 
-const renderFullScreenGallery = ( lang, dir ) => {
+const prefixClassname = 'wp-gallery-fullscreen',
+	renderFullScreenGallery = ( lang, dir ) => {
 		return `
-			<div class="wp-gallery-fullscreen" lang="${lang}" dir="${dir}">
-				<div class="wp-gallery-fullscreen-top">
-					<div class="wp-gallery-fullscreen-top-close"></div>
-				</div>
-				<div class="wp-gallery-fullscreen-main"></div>
+			<div class="${prefixClassname}" lang="${lang}" dir="${dir}">
+				<div class="${prefixClassname}-close"></div>
+				<div class="${prefixClassname}-main"></div>
 			</div>
 		`.trim()
 	},
 
 	hideFullscreenGallery = container => {
-		const fullscreenGallery = container.querySelector( '.wp-gallery-fullscreen' )
+		const fullscreenGallery = container.querySelector( `.${prefixClassname}` )
 		container.removeChild( fullscreenGallery )
 	},
 
 	showFullscreenGallery = ( mediaItems, selectedThumb, lang, dir, container = document.body ) => {
 
+		// render utils for fullscreen then slider component
 		container.insertAdjacentHTML( 'beforeend', renderFullScreenGallery( lang, dir ) )
-		container.querySelector( '.wp-gallery-fullscreen-main' )
+		container.querySelector( `.${prefixClassname}-main` )
 			.insertAdjacentHTML( 'beforeend', renderImageSlider( mediaItems, selectedThumb, lang, dir ) )
 
 		// onShow event for full screen component
-		const closeButton = container.querySelector( '.wp-gallery-fullscreen-top-close' )
+		const closeButton = container.querySelector( `.${prefixClassname}-close` )
 		closeButton.addEventListener( 'click', () => {
 			hideFullscreenGallery( container )
 		} )
