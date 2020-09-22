0a0042718d3408bdd94d8b6f7f0e867213bceb70
Eduardo <medina.eag@gmail.com>
2020-09-22 13:32:30 -0400

    Update preview header

--- src/preview.js
+++ src/preview.js
@@ -1,15 +1,21 @@
 import { msg } from './i18n'
 import '../style/preview.less'
 
-const renderPreview = ( lang, data, isTouch ) => {
+const getPreviewHeader = ( lang, imageUrl = '' ) =>{
+		return `
+			<div class="wikipediapreview-header">
+				${imageUrl ? `<div class="wikipediapreview-header-image" style="${`background-image:url('${imageUrl}');background-size:cover;`}"></div>` : ''}
+				<div class="wikipediapreview-header-wordmark wikipediapreview-header-wordmark-${lang}"></div>
+				<div class="wikipediapreview-header-closebtn"></div>
+			</div>
+	`.trim()
+	},
+
+	renderPreview = ( lang, data, isTouch ) => {
 		const imageUrl = data.imgUrl
 		return `
 			<div class="wikipediapreview${isTouch ? ' mobile' : ''}" lang="${lang}" dir="${data.dir}">
-				<div class="wikipediapreview-header">
-					<div class="wikipediapreview-header-image${imageUrl ? '' : '-wikipedia' }" style="${imageUrl && `background-image:url('${imageUrl}');background-size:cover;`}"></div>
-					<div class="wikipediapreview-header-wordmark wikipediapreview-header-wordmark-${lang}"></div>
-					<div class="wikipediapreview-header-closebtn"></div>
-				</div>
+				${getPreviewHeader( lang, imageUrl )}
 				<div class="wikipediapreview-body">
 					${data.extractHtml}
 					<div class="wikipediapreview-gallery">
@@ -26,11 +32,7 @@ const renderPreview = ( lang, data, isTouch ) => {
 	renderLoading = ( isTouch, lang, dir ) => {
 		return `
 			<div class="wikipediapreview${isTouch ? ' mobile' : ''}" lang="${lang}" dir="${dir}">
-					<div class="wikipediapreview-header">
-						<div class="wikipediapreview-loading-header-image"></div>
-						<div class="wikipediapreview-header-wordmark wikipediapreview-header-wordmark-${lang}"></div>
-						<div class="wikipediapreview-header-closebtn"></div>
-					</div>
+					${getPreviewHeader( lang )}
 					<div class="wikipediapreview-loading">
 						<div class="wikipediapreview-body">
 							<div class="wikipediapreview-loading-body-line larger"></div>
@@ -53,11 +55,7 @@ const renderPreview = ( lang, data, isTouch ) => {
 	renderError = ( isTouch, lang, title, dir ) => {
 		return `
 			<div class="wikipediapreview ${isTouch ? 'mobile' : ''}" lang="${lang}" dir="${dir}">
-					<div class="wikipediapreview-header">
-						<div class="wikipediapreview-loading-header-image"></div>
-						<div class="wikipediapreview-header-wordmark wikipediapreview-header-wordmark-${lang}"></div>
-						<div class="wikipediapreview-header-closebtn"></div>
-					</div>
+					${getPreviewHeader( lang )}
 					<div class="wikipediapreview-error">
 						<div class="wikipediapreview-error-body">
 							<div class="wikipediapreview-error-body-message">
@@ -76,11 +74,7 @@ const renderPreview = ( lang, data, isTouch ) => {
 	renderDisambiguation = ( isTouch, lang, title, dir ) => {
 		return `
 			<div class="wikipediapreview ${isTouch ? 'mobile' : ''}" lang="${lang}" dir="${dir}">
-					<div class="wikipediapreview-header">
-						<div class="wikipediapreview-loading-header-image"></div>
-						<div class="wikipediapreview-header-wordmark wikipediapreview-header-wordmark-${lang}"></div>
-						<div class="wikipediapreview-header-closebtn"></div>
-					</div>
+					${getPreviewHeader( lang )}
 					<div class="wikipediapreview-disambiguation">
 						<div class="wikipediapreview-disambiguation-body">
 							<div class="wikipediapreview-disambiguation-body-message">
@@ -88,7 +82,7 @@ const renderPreview = ( lang, data, isTouch ) => {
 								<span>${msg( lang, 'preview-disambiguation-message', title )}</span>
 							</div>
 							<div class="wikipediapreview-disambiguation-body-readon">
-								<a href=${`https://${lang}.m.wikipedia.org/wiki/${encodeURIComponent( title )}`} target="_blank">${msg( lang, 'read-on-wiki' )}</a>
+								<a href=${`https://${lang}.m.wikipedia.org/wiki/${encodeURIComponent( title )}`} target="_blank">${msg( lang, 'read-on-wiki-disambiguation' )}</a>
 							</div>
 						</div>
 					</div>
