7fd4a344185b92fc9b4925c4ab4a3f2b623cc94e
narayan <narayan3119@gmail.com>
2021-03-30 01:50:17 +0530

    created different pages

---
+++ cypress/integration/examples/previewPage.js
@@ -0,0 +1,73 @@
+import { Homepage } from "../pageObject/HomePage";
+const homepage = new Homepage();
+
+describe("Wikimedia preview page test", function () {
+  before(function () {
+    homepage.getHomePage("/");
+    cy.fixture("example").then(function (data) {
+      this.data = data;
+    });
+  });
+  it("Check the working of all the articles opening", function () {
+    // cy.visit("/");
+    this.data.airticleName.forEach(function (element) {
+      cy.openingArticle(element);
+    });
+  });
+  it("Check Header", function () {
+    homepage.getHomePage("/");
+    cy.homePageHeader().should("have.text", "Wikipedia Preview demo");
+    cy.homePageHeader().should("have.css", "font-size", "32px");
+  });
+// it("Check Body", () => {
+//   cy.bodyTitle().should("have.text", "Wildlife of the Central African Republic")
+//   cy.bodySubTitle().should("have.text", "Language · English")
+//   // cy.bodyImage().image()
+//   // cy.homePageBody().a()
+// })
+// it("Check next page", () => {
+//   cy.bodyTitle().should("have.text", "Gili Trawangan")
+//   cy.bodySubTitle().should("have.text", "Language · Bahasa Indonesia")
+//   // cy.bodyImage().image()
+//   // cy.homePageBody().a()
+// })
+// it("Check next page", () => {
+//   cy.bodyTitle().should("have.text", "विस्तार से जानिये कालिंजर दुर्ग के बारे में")
+//   cy.bodySubTitle().should("have.text", "Language · Hindi")
+//   // cy.bodyImage().image()
+//   // cy.homePageBody().a()
+// })
+// it("Check next page", () => {
+//   cy.bodyTitle().should("have.text", "เฉลิมชัย โฆษิตพิพัฒน์")
+//   cy.bodySubTitle().should("have.text", "Language · Thai")
+//   // cy.bodyImage().image()
+//   // cy.homePageBody().a()
+// })
+// it("Check next page", () => {
+//   cy.bodyTitle().should("have.text", "Wildlife of the Central African Republic")
+//   cy.bodySubTitle().should("have.text", "Language · Spanish")
+// //   cy.bodyImage().image()
+// //   cy.homePageBody().a()
+// })
+// it("Check next page", () => {
+//   cy.bodyTitle().should("have.text", "Wildlife of the Central African Republic")
+//   cy.bodySubTitle().should("have.text", "Language · French")
+//   // cy.bodyImage().image()
+//   // cy.homePageBody().a()
+// })
+// it("Check next page", () => {
+//   cy.bodyTitle().should("have.text", "Wildlife of the Central African Republic")
+//   cy.bodySubTitle().should("have.text", "Language · Arabic")
+//   // cy.bodyImage().image()
+//   // cy.homePageBody().a()
+// })
+
+  it("Check Footer", () => {
+    cy.pageFooter().should("be.visible");
+    cy.get("body > div.footer > p") .should("have.css", "font-size", "12px");
+  });
+
+ 
+});
+
+
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,14 +1,104 @@
-import {Homepage} from "../integration/pageObject/HomePage"
-const homepage = new Homepage()
+import { Homepage } from "../integration/pageObject/HomePage";
+const homepage = new Homepage();
 
+Cypress.Commands.add("openingArticle", (articleName) => {
+  cy.get("div.title").each(($el, index, $list) => {
+    if ($el.text().includes(articleName)) {
+      cy.get("div.title").eq(index)
+      homepage.getHomePage("/");
+      // cy.go('back')
+    }
+  });
+});
+Cypress.Commands.add("homePageHeader", () => {
+  cy.get("body > div.header");
+});
 
- Cypress.Commands.add("openingArticle", (articleName) => { 
-    cy.get('div.title').each(($el,index,$list) => {
-        if($el.text().includes(articleName))
-        {
-            cy.get('div.title').eq(index).click()
-            homepage.getHomePage("/")
-            // cy.go('back')
-        }
-    })
- })
+Cypress.Commands.add("pageHeader", () => {
+  cy.get("body > a > div");
+});
+Cypress.Commands.add("pageTitle", () => {
+  cy.get("body > div.container > div.title > p");
+});
+Cypress.Commands.add("pageFooter", () => {
+  cy.get("body > div.footer");
+});
+Cypress.Commands.add("bodyTitle", () => {
+  cy.get("body> div.container > div.listview > div.item > a > div.title");
+});
+Cypress.Commands.add("bodyImage", () => {
+  cy.get("body> div.container > div.listview > div.item > a > div.image");
+});
+
+Cypress.Commands.add("bodySubTitle", () => {
+  cy.get("body> div.container > div.listview > div.item > a > div.image > div.subtitle");
+});
+
+Cypress.Commands.add("bodyUrl", () => {
+    cy.get("body> div.container > div.listview > div.item > a ");
+  });
+Cypress.Commands.add("para1", () => {
+  cy.get("body > div.container > div.content > p:nth-child(1)"); //paragraph 1
+});
+
+Cypress.Commands.add("para2", () => {
+  cy.get("body > div.container > div.content > p:nth-child(2)"); //paragraph 2
+});
+
+Cypress.Commands.add("para3", () => {
+  cy.get("body > div.container > div.content > p:nth-child(3)"); //paragraph 3
+});
+
+Cypress.Commands.add("para4", () => {
+  cy.get("body > div.container > div.content > p:nth-child(4)"); //paragraph 3
+});
+
+Cypress.Commands.add("coverImage", () => {
+  cy.get("body > div.container > div.cover", { timeout: 5000 });
+});
+
+Cypress.Commands.add("previewBox", () => {
+  cy.get("body > div.wp-popup");
+});
+
+Cypress.Commands.add("previewBoxHeaderImg", () => {
+  cy.get(
+    "body > div.wp-popup > div > div.wikipediapreview-header > div.wikipediapreview-header-image",
+    { timeout: 5000 }
+  );
+});
+
+Cypress.Commands.add("continueReadBtn", () => {
+  cy.get("body > div.wp-popup > div > div.wikipediapreview-footer > span");
+});
+
+Cypress.Commands.add("previewBoxFooterImg1", () => {
+  cy.get(
+    "body > div.wp-popup > div > div.wikipediapreview-body > div > div > div:nth-child(1)",
+    { timeout: 5000 }
+  );
+});
+
+Cypress.Commands.add("previewBoxFooterImg2", () => {
+  cy.get(
+    "body > div.wp-popup > div > div.wikipediapreview-body > div > div > div:nth-child(2)",
+    { timeout: 5000 }
+  );
+});
+
+Cypress.Commands.add("previewBoxFooterImg3", () => {
+  cy.get(
+    "body > div.wp-popup > div > div.wikipediapreview-body > div > div > div:nth-child(3)",
+    { timeout: 5000 }
+  );
+});
+
+Cypress.Commands.add("readMoreBtn", () => {
+  cy.get("body > div.wp-popup > div > div.wikipediapreview-footer > a");
+});
+
+Cypress.Commands.add("previewBoxCloseBtn", () => {
+  cy.get(
+    "body > div.wp-popup > div > div.wikipediapreview-header > div.wikipediapreview-header-closebtn"
+  );
+});
