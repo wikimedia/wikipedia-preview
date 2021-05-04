5e145d08a4401c3bcc57e8708e6c1da1d0f1fea7
Eduardo Medina <medina.eag@gmail.com>
2021-05-04 13:27:08 -0400

    T280061: Update eslint-config-wikimedia to 0.20 (#112)
    * Update eslint-config-wikimedia to 0.20
    
    * No shadow

--- src/cachedRequest.js
+++ src/cachedRequest.js
@@ -1,29 +1,29 @@
 const request = ( url, callback ) => {
-		const xhr = new XMLHttpRequest()
-		xhr.open( 'GET', url )
-		xhr.send()
-		xhr.addEventListener( 'load', () => {
-			callback( JSON.parse( xhr.responseText ) )
-		} )
-		xhr.addEventListener( 'error', () => {
-			callback( null, xhr.status )
-		} )
-	},
+	const xhr = new XMLHttpRequest()
+	xhr.open( 'GET', url )
+	xhr.send()
+	xhr.addEventListener( 'load', () => {
+		callback( JSON.parse( xhr.responseText ) )
+	} )
+	xhr.addEventListener( 'error', () => {
+		callback( null, xhr.status )
+	} )
+}
 
-	dataCache = {},
+const dataCache = {}
 
-	cachedRequest = ( url, transformFn, callback, r = request ) => {
-		if ( dataCache[ url ] !== undefined ) {
-			callback( dataCache[ url ] )
-			return
-		}
-		r( url, data => {
-			if ( data ) {
-				callback( dataCache[ url ] = transformFn( data ) )
-			} else {
-				callback( false )
-			}
-		} )
+const cachedRequest = ( url, transformFn, callback, r = request ) => {
+	if ( dataCache[ url ] !== undefined ) {
+		callback( dataCache[ url ] )
+		return
 	}
+	r( url, data => {
+		if ( data ) {
+			callback( dataCache[ url ] = transformFn( data ) )
+		} else {
+			callback( false )
+		}
+	} )
+}
 
 export { cachedRequest }
