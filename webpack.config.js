9d19630c663bc9396bd380031281dd432ce97ba3
Huei Tan <huei90@gmail.com>
2021-05-10 17:48:12 +0200

    Bring link.less / link.css back into dist

--- webpack.config.js
+++ webpack.config.js
@@ -111,7 +111,7 @@ const config = {
         ]
       },
       {
-        test: /\.css$/i,
+        test: /link.less$/i,
         use: [
             MiniCssExtractPlugin.loader,
             'css-loader',
