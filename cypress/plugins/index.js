63e1400fb2aeb89349d5ef207dbd811ee88bbaa0
shailesh1028 <shaileshkanojiya1998@iitkgp.ac.in>
2021-06-23 23:51:33 +0530

    PR Review Changes

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,4 +1,4 @@
-/// <reference types="cypress" />
+// / <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
 //
@@ -16,7 +16,7 @@
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+module.exports = ( on, config ) => {
+	// `on` is used to hook into various events Cypress emits
+	// `config` is the resolved Cypress config
 }
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,147 +3,171 @@ import { msg } from '../../src/i18n'
 
 const preview = new Preview()
 
-Cypress.Commands.add('navigateToHomePage', (url='') => {
-  cy.visit(''+url)
-})
+Cypress.Commands.add( 'navigateToHomePage', ( url = '' ) => {
+	cy.visit( String( url ) )
+} )
 
+Cypress.Commands.add( 'navigateToMobileHomePage', ( url = '' ) => {
+    cy.visitMobile(String( url ))
+} )
 
 // Custom Command to check the existence of Preview
-Cypress.Commands.add('CheckPreview' , () =>{
+Cypress.Commands.add( 'CheckPreview', () =>{
 
-    preview.Preview().should('be.visible')
+	preview.getPreview().should( 'be.visible' )
 
-    preview.Header().should('be.visible')
-    preview.HeaderWatermark().should('be.visible')
-    preview.HeaderClosebtn().should('be.visible')
+	preview.getHeader().should( 'be.visible' )
+	preview.getHeaderWatermark().should( 'be.visible' )
+	preview.getHeaderClosebtn().should( 'be.visible' )
 
-    preview.Body ().should('be.visible')
+	preview.getBody().should( 'be.visible' )
 
-})
+} )
 
 // Custom Command to check the Preview in Standard With Image Mode
-Cypress.Commands.add('CheckPreviewStandardWithImage' , () =>{
+Cypress.Commands.add( 'CheckPreviewStandardWithImage', () =>{
 
-    preview.Preview().should('be.visible')
+	preview.getPreview().should( 'be.visible' )
 
-    preview.Header().should('be.visible')
-    preview.HeaderImage().should('be.visible')
-    preview.HeaderWatermark().should('be.visible')
-    preview.HeaderClosebtn().should('be.visible')
+	preview.getHeader().should( 'be.visible' )
+	preview.getHeaderImage().should( 'be.visible' )
+	preview.getHeaderWatermark().should( 'be.visible' )
+	preview.getHeaderClosebtn().should( 'be.visible' )
 
-    preview.Body ().should('be.visible')
-    preview.BodyContent().should('be.visible')
-    .and('have.css','font-size').and('equal','18px')
+	preview.getBody().should( 'be.visible' )
+	preview.getBodyContent().should( 'be.visible' )
+		.and( 'have.css', 'font-size' ).and( 'equal', '18px' )
 
-    preview.Footer().should('be.visible')
-    preview.FooterContiReading().should('be.visible')
+	preview.getFooter().should( 'be.visible' )
+	preview.getFooterContiReading().should( 'be.visible' )
 
-})
+} )
 
 // Custom Command to check the Preview in Standard Without Image Mode
-Cypress.Commands.add('CheckPreviewStandardWithoutImage' , () =>{
+Cypress.Commands.add( 'CheckPreviewStandardWithoutImage', () =>{
 
-    preview.Preview().should('be.visible')
+	preview.getPreview().should( 'be.visible' )
 
-    preview.Header().should('be.visible')
-    preview.HeaderWatermark().should('be.visible')
-    preview.HeaderClosebtn().should('be.visible')
+	preview.getHeader().should( 'be.visible' )
+	preview.getHeaderWatermark().should( 'be.visible' )
+	preview.getHeaderClosebtn().should( 'be.visible' )
 
-    preview.Body ().should('be.visible')
-    preview.BodyContent().should('be.visible')
-    .and('have.css','font-size').and('equal','18px')
+	preview.getBody().should( 'be.visible' )
+	preview.getBodyContent().should( 'be.visible' )
+		.and( 'have.css', 'font-size' ).and( 'equal', '18px' )
 
-    preview.Footer().should('be.visible')
-    preview.FooterContiReading().should('be.visible')
+	preview.getFooter().should( 'be.visible' )
+	preview.getFooterContiReading().should( 'be.visible' )
 
-})
+} )
+
+// Custom Command to check the Preview in Standard Without Image Mode
+Cypress.Commands.add( 'CheckPreviewExpanded', () =>{
+
+	preview.getPreview().should( 'be.visible' )
+		.and( 'have.class', 'expanded' )
+
+	preview.getHeader().should( 'be.visible' )
+	preview.getHeaderWatermark().should( 'be.visible' )
+	preview.getHeaderClosebtn().should( 'be.visible' )
+
+	preview.getBody().should( 'be.visible' )
+	preview.getBodyContent().should( 'be.visible' )
+		.and( 'have.css', 'font-size' ).and( 'equal', '18px' )
+	preview.getHeaderImage().its('length').then( len => {
+        if(len>0){
+           preview.getBodyGallery().scrollIntoView().should('be.visible')
+           preview.getBodyGalleryImages().should('be.visible')
+        }
+    })
+	preview.getFooter().should( 'be.visible' )
+	preview.getFooterReadMore().should( 'be.visible' )
+
+} )
 
 // Custom Command to check the Preview in Disambiguation Mode
-Cypress.Commands.add('CheckPreviewDisambiguation' , () =>{
+Cypress.Commands.add( 'CheckPreviewDisambiguation', () =>{
 
-    preview.Preview().should('be.visible')
+	preview.getPreview().should( 'be.visible' )
 
-    preview.Header().should('be.visible')
-    preview.HeaderWatermark().should('be.visible')
-    preview.HeaderClosebtn().should('be.visible')
+	preview.getHeader().should( 'be.visible' )
+	preview.getHeaderWatermark().should( 'be.visible' )
+	preview.getHeaderClosebtn().should( 'be.visible' )
 
-    preview.Preview().then( tag => {
+	preview.getPreview().then( tag => {
 
-        const lang = tag.attr('lang')
+		const lang = tag.attr( 'lang' )
 
-        preview.Body().should('be.visible')
-        .and('have.class','wikipediapreview-body-disambiguation')
-        .and('have.css','font-size').and('equal','16px')
+		preview.getBody().should( 'be.visible' )
+			.and( 'have.class', 'wikipediapreview-body-disambiguation' )
+			.and( 'have.css', 'font-size' ).and( 'equal', '16px' )
 
-        preview.BodyMessage().then( tag => {
-            cy.wrap(tag).find('div.wikipediapreview-body-icon')
-        })
+		preview.getBodyMessage().then( tag => {
+			cy.wrap( tag ).find( 'div.wikipediapreview-body-icon' )
+		} )
 
-        preview.BodyAction().should('be.visible')
-        .and('contain',msg( lang, 'read-on-wiki' ))
-    })
+		preview.getBodyAction().should( 'be.visible' )
+			.and( 'contain', msg( lang, 'read-on-wiki' ) )
+	} )
 
-})
+} )
 
 // Custom Command to check the Preview in Offline Mode
-Cypress.Commands.add('CheckPreviewOffline' , () =>{
+Cypress.Commands.add( 'CheckPreviewOffline', () =>{
 
-	preview.Preview().should('be.visible')
+	preview.getPreview().should( 'be.visible' )
 
-    preview.Header().should('be.visible')
-    preview.HeaderWatermark().should('be.visible')
-    preview.HeaderClosebtn().should('be.visible')
+	preview.getHeader().should( 'be.visible' )
+	preview.getHeaderWatermark().should( 'be.visible' )
+	preview.getHeaderClosebtn().should( 'be.visible' )
 
-    preview.Preview().then( tag => {
+	preview.getPreview().then( tag => {
 
-        const lang = tag.attr('lang')
+		const lang = tag.attr( 'lang' )
 
-        preview.Body().should('be.visible')
-        .and('have.class','wikipediapreview-body-disambiguation')
-        .and('have.css','font-size').and('equal','16px')
+		preview.getBody().should( 'be.visible' )
+			.and( 'have.class', 'wikipediapreview-body-error' )
+			.and( 'have.css', 'font-size' ).and( 'equal', '16px' )
 
-        preview.BodyMessage().then( tag => {
-            cy.wrap(tag).find('div.wikipediapreview-body-icon').should('be.visible')
-            cy.wrap(tag).find('span').should('contain',msg( lang, 'preview-offline-message' ))
+		preview.getBodyMessage().then( tag => {
+			cy.wrap( tag ).find( 'div.wikipediapreview-body-icon' ).should( 'be.visible' )
+			cy.wrap( tag ).find( 'span' ).should( 'contain', msg( lang, 'preview-offline-message' ) )
 
-        })
+		} )
 
-        preview.BodyAction().should('be.visible')
-        .and('contain',msg( lang, 'preview-offline-cta' ))
+		preview.getBodyAction().should( 'be.visible' )
+			.and( 'contain', msg( lang, 'preview-offline-cta' ) )
 
+	} )
 
-    })
-
-})
+} )
 
 // Custom Command to check the Preview in Error Mode
-Cypress.Commands.add('CheckPreviewError' , () =>{
+Cypress.Commands.add( 'CheckPreviewError', () =>{
 
-	preview.Preview().should('be.visible')
+	preview.getPreview().should( 'be.visible' )
 
-    preview.Header().should('be.visible')
-    preview.HeaderWatermark().should('be.visible')
-    preview.HeaderClosebtn().should('be.visible')
+	preview.getHeader().should( 'be.visible' )
+	preview.getHeaderWatermark().should( 'be.visible' )
+	preview.getHeaderClosebtn().should( 'be.visible' )
 
-    preview.Preview().then( tag => {
+	preview.getPreview().then( tag => {
 
-        const lang = tag.attr('lang')
+		const lang = tag.attr( 'lang' )
 
-        preview.Body().should('be.visible')
-        .and('have.class','wikipediapreview-body-disambiguation')
-        .and('have.css','font-size').and('equal','16px')
+		preview.getBody().should( 'be.visible' )
+			.and( 'have.class', 'wikipediapreview-body-disambiguation' )
+			.and( 'have.css', 'font-size' ).and( 'equal', '16px' )
 
-        preview.BodyMessage().then( tag => {
-            cy.wrap(tag).find('div.wikipediapreview-body-icon')
-            cy.wrap(tag).find('span').should('contain',msg( lang, 'preview-error-message' ))
+		preview.getBodyMessage().then( tag => {
+			cy.wrap( tag ).find( 'div.wikipediapreview-body-icon' )
+			cy.wrap( tag ).find( 'span' ).should( 'contain', msg( lang, 'preview-error-message' ) )
 
-        })
+		} )
 
-        preview.BodyAction().should('be.visible')
-        .should('contain',msg( lang, 'read-on-wiki' ))
+		preview.getBodyAction().should( 'be.visible' )
+			.should( 'contain', msg( lang, 'read-on-wiki' ) )
 
+	} )
 
-    })
-
-})
-
+} )
