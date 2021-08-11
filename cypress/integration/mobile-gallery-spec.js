2b6c6bedbe2dc174e32113aaf28679682b043834
Shailesh <41967175+shailesh1028@users.noreply.github.com>
2021-08-12 01:50:45 +0530

    Removed (res>1) condition in gallery images (#140)

--- cypress/integration/mobile-gallery-spec.js
+++ cypress/integration/mobile-gallery-spec.js
@@ -21,21 +21,19 @@ describe( 'Check the Gallery Pages in Mobile View', () => {
 		} )
 		const i = 0
 		// Check if the Images exist
-		preview.getBodyGalleryImages().its( 'length' ).then( res => {
-			if ( res > 1 ) {
-				// Open the Gallery
-				preview.getBodyGalleryImages().first().click()
-				// Check the Gallery Page
-				gallery.checkGalleryPage( i )
-				// Click on the Next Icon
-				gallery.getGalleryNextBtn().click()
-				// Click on the Previous Icon
-				gallery.getGalleryPrevBtn().click()
-				// Close the Gallery
-				gallery.getGalleryCloseBtn().click()
-				// Close the Preview
-				preview.getHeaderClosebtn().click()
-			}
+		preview.getBodyGalleryImages().its( 'length' ).then( () => {
+			// Open the Gallery
+			preview.getBodyGalleryImages().first().click()
+			// Check the Gallery Page
+			gallery.checkGalleryPage( i )
+			// Click on the Next Icon
+			gallery.getGalleryNextBtn().click()
+			// Click on the Previous Icon
+			gallery.getGalleryPrevBtn().click()
+			// Close the Gallery
+			gallery.getGalleryCloseBtn().click()
+			// Close the Preview
+			preview.getHeaderClosebtn().click()
 		} )
 	} )
 
@@ -50,46 +48,15 @@ describe( 'Check the Gallery Pages in Mobile View', () => {
 		} )
 		const i = 0
 		// Check if the Images exist
-		preview.getBodyGalleryImages().its( 'length' ).then( res => {
-			if ( res > 1 ) {
-				// Open the Gallery
-				preview.getBodyGalleryImages().first().click()
-				// Check the Gallery Page
-				gallery.checkGalleryPage( i )
-				// Swipe to go Next
-				gallery.getGalleryImage( i ).swipe( 'right', 'left' )
-				// Swipe to go Previous
-				gallery.getGalleryImage( i + 1 ).swipe( 'left', 'right' )
-			}
-		} )
-	} )
-
-	it( 'Check the Gallery Image Caption Scrollable', () => {
-		// Open the Preview
-		preview.getPreviewSpan().eq( 4 ).click()
-		// Check for the Continue Reading exist
-		preview.getFooterContinueReading().then( res => {
-			if ( res.css( 'display' ) !== 'none' ) {
-				preview.getFooterContinueReading().click()
-			}
-		} )
-		const i = 1
-		// Check if the Images exist
-		preview.getBodyGalleryImages().its( 'length' ).then( res => {
-			if ( res > 1 ) {
-				// Open the Gallery
-				preview.getBodyGalleryImages().eq( 1 ).click()
-				// Check the Gallery Page
-				gallery.checkGalleryPage( i )
-				// Check if the Caption Bar exist
-				gallery.getGalleryCaptionBar( i ).its( 'length' ).then( len => {
-					if ( len > 0 ) {
-						// Scroll the Caption Text
-						gallery.getGalleryCaption( i ).swipe( 'bottom', 'center' )
-						// gallery.getGalleryCaptionText(i).scrollTo('bottom')
-					}
-				} )
-			}
+		preview.getBodyGalleryImages().its( 'length' ).then( () => {
+			// Open the Gallery
+			preview.getBodyGalleryImages().first().click()
+			// Check the Gallery Page
+			gallery.checkGalleryPage( i )
+			// Swipe to go Next
+			gallery.getGalleryImage( i ).swipe( 'right', 'left' )
+			// Swipe to go Previous
+			gallery.getGalleryImage( i + 1 ).swipe( 'left', 'right' )
 		} )
 	} )
 
@@ -104,21 +71,18 @@ describe( 'Check the Gallery Pages in Mobile View', () => {
 		} )
 		const i = 1
 		// Check if the Images exist
-		preview.getBodyGalleryImages().its( 'length' ).then( res => {
-			if ( res > 1 ) {
-				// Open the Gallery
-				preview.getBodyGalleryImages().eq( 1 ).click()
-				// Check the Gallery Page
-				gallery.checkGalleryPage( i )
-				// Check if the Caption Bar exist
-				gallery.getGalleryCaptionBar( i ).its( 'length' ).then( len => {
-					if ( len > 0 ) {
-						// Scroll the Caption Text
-						gallery.getGalleryCaptionBar( i ).click()
-						// gallery.getGalleryCaptionText(i).scrollTo('bottom')
-					}
-				} )
-			}
+		preview.getBodyGalleryImages().its( 'length' ).then( () => {
+			// Open the Gallery
+			preview.getBodyGalleryImages().eq( 1 ).click()
+			// Check the Gallery Page
+			gallery.checkGalleryPage( i )
+			// Check if the Caption Bar exist
+			gallery.getGalleryCaptionBar( i ).its( 'length' ).then( len => {
+				if ( len > 0 ) {
+					// Scroll the Caption Text
+					gallery.getGalleryCaptionBar( i ).click()
+				}
+			} )
 		} )
 	} )
 
