9cff40e5a502d4ae617c97b7c4a2bafef65b3643
ci-build <ci-build@wikimedia.org>
2021-08-11 20:24:43 +0000

    Update demo [ci skip]

--- cypress/integration/gallery-spec.js
+++ cypress/integration/gallery-spec.js
@@ -21,15 +21,13 @@ describe( 'Check the Gallery Pages ', () => {
 		} )
 		const i = 0
 		// Check if the Images exist
-		preview.getBodyGalleryImages().its( 'length' ).then( res => {
-			if ( res > 0 ) {
-				// Open the Gallery and Close it
-				preview.getBodyGalleryImages().first().click()
-				// Check the Gallery Page
-				gallery.checkGalleryPage( i )
-				// Close the Gallery
-				gallery.getGalleryCloseBtn().click()
-			}
+		preview.getBodyGalleryImages().its( 'length' ).then( () => {
+			// Open the Gallery and Close it
+			preview.getBodyGalleryImages().first().click()
+			// Check the Gallery Page
+			gallery.checkGalleryPage( i )
+			// Close the Gallery
+			gallery.getGalleryCloseBtn().click()
 		} )
 	} )
 
@@ -44,23 +42,21 @@ describe( 'Check the Gallery Pages ', () => {
 		} )
 		const i = 0
 		// Check if the Image exist
-		preview.getBodyGalleryImages().its( 'length' ).then( res => {
-			if ( res > 1 ) {
-				// Open the Gallery
-				preview.getBodyGalleryImages().first().click()
-				// Check the Current Gallery Page
-				gallery.checkGalleryPage( i )
-				// Click on the Next Icon
-				gallery.getGalleryNextBtn().click()
-				// Check the Next Gallery Page
-				gallery.checkGalleryPage( i + 1 )
-				// Click on the Previous Icon
-				gallery.getGalleryPrevBtn().click()
-				// Check the Previous Gallery Page
-				gallery.checkGalleryPage( i )
-				// Close the Gallery
-				gallery.getGalleryCloseBtn().click()
-			}
+		preview.getBodyGalleryImages().its( 'length' ).then( () => {
+			// Open the Gallery
+			preview.getBodyGalleryImages().first().click()
+			// Check the Current Gallery Page
+			gallery.checkGalleryPage( i )
+			// Click on the Next Icon
+			gallery.getGalleryNextBtn().click()
+			// Check the Next Gallery Page
+			gallery.checkGalleryPage( i + 1 )
+			// Click on the Previous Icon
+			gallery.getGalleryPrevBtn().click()
+			// Check the Previous Gallery Page
+			gallery.checkGalleryPage( i )
+			// Close the Gallery
+			gallery.getGalleryCloseBtn().click()
 		} )
 
 	} )
