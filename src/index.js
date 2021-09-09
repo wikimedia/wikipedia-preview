2ecfd2c696ad39d8d5a48a8cc283dd9f21ee427f
Eduardo <medina.eag@gmail.com>
2021-09-09 23:03:00 +0200

    Update getPreviewHtml params

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const invokeCallback = ( events, name, params ) => {
 
 // getPreviewHtml is meant to be used by the Wordpress plugin only
 const getPreviewHtml = ( title, lang, callback ) => {
-	requestPagePreview( lang, title, isTouch, data => {
+	requestPagePreview( lang, title, data => {
 		callback( renderPreview( lang, data, isTouch ) )
 	} )
 }
