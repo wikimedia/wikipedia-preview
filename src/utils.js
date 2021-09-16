222226f985fda6bfbc3d328891839ff304fc8056
Stephane Bisson <stephane.c.bisson@gmail.com>
2021-09-16 04:48:31 -0400

    Remove invisible spans from HTML (#148)
    Co-authored-by: Stephane Bisson <sbisson@wikimedia.org>

--- src/utils.js
+++ src/utils.js
@@ -50,6 +50,11 @@ const convertUrlToMobile = url => {
 
 const strip = html => {
 	const doc = new window.DOMParser().parseFromString( html, 'text/html' )
+	for ( const span of doc.querySelectorAll( 'span' ) ) {
+		if ( span.style.display === 'none' ) {
+			span.remove()
+		}
+	}
 	return doc.body.textContent || ''
 }
 
