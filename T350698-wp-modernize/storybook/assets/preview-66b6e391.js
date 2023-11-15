a77e56fec1852f71f0499649664f68f27ebf6256
medied <medied@users.noreply.github.com>
2023-11-15 20:23:36 +0000

    Deploying to gh-pages from @ wikimedia/wikipedia-preview@b3942554c83b12dc8c77fe73b26542b5565f7694 🚀

--- T350698-wp-modernize/storybook/assets/preview-66b6e391.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./iframe-7ef8fa81.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-1f6ff66b.js").then(e=>e.D),["./DocsRenderer-NNNQARDV-1f6ff66b.js","./iframe-7ef8fa81.js","./index-131b54fa.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-66b6e391.js.map
