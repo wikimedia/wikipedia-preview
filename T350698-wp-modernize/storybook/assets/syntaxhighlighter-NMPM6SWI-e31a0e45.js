15910566aec93a7a6f58eee86c8a7353573d6bbd
medied <medied@users.noreply.github.com>
2023-11-15 20:09:28 +0000

    Deploying to gh-pages from @ wikimedia/wikipedia-preview@1e8b24b0c2293fff4a5d7bda035317971cb451fe 🚀

---
+++ T350698-wp-modernize/storybook/assets/syntaxhighlighter-NMPM6SWI-e31a0e45.js
@@ -0,0 +1,2 @@
+import{S as e,c as s,s as c}from"./DocsRenderer-NNNQARDV-1f6ff66b.js";import"./iframe-7ef8fa81.js";import"../sb-preview/runtime.js";import"./index-131b54fa.js";import"./index-d37d4223.js";import"./index-356e4a49.js";export{e as SyntaxHighlighter,s as createCopyToClipboardFunction,c as default};
+//# sourceMappingURL=syntaxhighlighter-NMPM6SWI-e31a0e45.js.map
