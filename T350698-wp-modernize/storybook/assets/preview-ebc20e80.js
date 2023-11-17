524672e11e904ae9b9a347a920aa719e7bd461a8
hueitan <hueitan@users.noreply.github.com>
2023-11-17 11:28:59 +0000

    Deploying to gh-pages from @ wikimedia/wikipedia-preview@39193f6e2c830fcc25c020dd675892e9b799b87a 🚀

--- T350698-wp-modernize/storybook/assets/preview-ebc20e80.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./iframe-e7f0c983.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-8dca7ae7.js").then(e=>e.D),["./DocsRenderer-NNNQARDV-8dca7ae7.js","./iframe-e7f0c983.js","./index-131b54fa.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-ebc20e80.js.map
--- T350698-wp-modernize/storybook/assets/syntaxhighlighter-NMPM6SWI-3e86997f.js
+++
@@ -1,2 +0,0 @@
-import{S as e,c as s,s as c}from"./DocsRenderer-NNNQARDV-8dca7ae7.js";import"./iframe-e7f0c983.js";import"../sb-preview/runtime.js";import"./index-131b54fa.js";import"./index-d37d4223.js";import"./index-356e4a49.js";export{e as SyntaxHighlighter,s as createCopyToClipboardFunction,c as default};
-//# sourceMappingURL=syntaxhighlighter-NMPM6SWI-3e86997f.js.map
