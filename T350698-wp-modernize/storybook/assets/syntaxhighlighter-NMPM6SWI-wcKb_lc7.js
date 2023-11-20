248c2a0a1d91d39b5419c4f42402e140cd0caef4
hueitan <hueitan@users.noreply.github.com>
2023-11-20 20:04:01 +0000

    Deploying to gh-pages from @ wikimedia/wikipedia-preview@45f6bdb314ba1f30df00a15ada7a9b1df08c2914 🚀

---
+++ T350698-wp-modernize/storybook/assets/syntaxhighlighter-NMPM6SWI-wcKb_lc7.js
@@ -0,0 +1,2 @@
+import{S as e,c as s,s as c}from"./DocsRenderer-NNNQARDV-W0W1PvB1.js";import"./iframe-VG9jwyZC.js";import"../sb-preview/runtime.js";import"./index-HZNADR7h.js";import"./index-aKxH7ZY0.js";import"./index-PPLHz8o0.js";export{e as SyntaxHighlighter,s as createCopyToClipboardFunction,c as default};
+//# sourceMappingURL=syntaxhighlighter-NMPM6SWI-wcKb_lc7.js.map
