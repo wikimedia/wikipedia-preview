b9d62c6ea17da72d6db3327a24c0b9b2e540ca6d
eamedina <eamedina@wikimedia.org>
2023-11-21 15:42:38 -0400

    Linting

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'cypress'
 
-export default defineConfig({
-  defaultCommandTimeout: 8000,
-  chromeWebSecurity: false,
-  e2e: {
-    baseUrl: 'http://localhost:5173/', // your localhost port in vite (5173 is the default for vite server)
-  },
-})
+export default defineConfig( {
+	defaultCommandTimeout: 8000,
+	chromeWebSecurity: false,
+	e2e: {
+		baseUrl: 'http://localhost:5173/' // your localhost port in vite (5173 is the default for vite server)
+	}
+} )
