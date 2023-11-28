8e30d24f7a54c80fc3bf42c46d365c73479db3d0
hueitan <hueitan@users.noreply.github.com>
2023-11-28 11:52:22 +0000

    Deploying to gh-pages from @ wikimedia/wikipedia-preview@fd81d715dd1b999b2b6668863cba2b2a5e855739 🚀

---
+++ T350698-wp-modernize/storybook/assets/preview-vZrJWRAk.js
@@ -0,0 +1,8 @@
+import{_ as t}from"./iframe-CgUR5xgw.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-MILN3Ei-.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4]),import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-vZrJWRAk.js.map
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-MILN3Ei-.js","./iframe-CgUR5xgw.js","./index-HZNADR7h.js","./index-aKxH7ZY0.js","./index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
