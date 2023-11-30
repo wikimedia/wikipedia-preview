c4e53c767280a75084c6a2ccd1d44b44ab0a8792
Huei Tan <huei90@gmail.com>
2023-11-30 10:29:34 +0100

    update demo.sh script

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -5,4 +5,3 @@ mkdir -p demo/script
 cp dist/wikipedia-preview.umd.cjs demo/script
 cp dist/wikipedia-preview.css demo/css
 sed -i -e 's/\/dist\/wikipedia-preview\.umd\.cjs/\.\.\/script\/wikipedia-preview\.umd\.cjs/g' demo/articles/*.html
-sed -i -e 's/\/dist\/wikipedia-preview.css/\.\.\/css\/wikipedia-preview\.css/g' demo/articles/*.html
