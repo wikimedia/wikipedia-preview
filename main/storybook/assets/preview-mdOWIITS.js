3e7539cdab7d4a9d607a526e5087db79cbf3f24e
medied <medina.eag@gmail.com>
2023-12-06 21:01:35 +0000

    Deploying to gh-pages from @ wikimedia/wikipedia-preview@2d79811d0a8be078b7889bb7ca88d212564b7710 🚀

--- main/storybook/assets/preview-mdOWIITS.js
+++
@@ -1,8 +0,0 @@
-import{_ as t}from"./iframe-Zv2b-0fQ.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-7f55ifUB.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4]),import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-mdOWIITS.js.map
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-7f55ifUB.js","./iframe-Zv2b-0fQ.js","./index-HZNADR7h.js","./index-aKxH7ZY0.js","./index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
--- main/storybook/assets/syntaxhighlighter-NMPM6SWI-LXMEGBgb.js
+++
@@ -1,2 +0,0 @@
-import{S as e,c as s,s as c}from"./DocsRenderer-NNNQARDV-7f55ifUB.js";import"./iframe-Zv2b-0fQ.js";import"../sb-preview/runtime.js";import"./index-HZNADR7h.js";import"./index-aKxH7ZY0.js";import"./index-PPLHz8o0.js";export{e as SyntaxHighlighter,s as createCopyToClipboardFunction,c as default};
-//# sourceMappingURL=syntaxhighlighter-NMPM6SWI-LXMEGBgb.js.map
