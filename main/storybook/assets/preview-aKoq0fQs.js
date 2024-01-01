479dfe4841e761e2ee78809a3554a93e050e1445
translatewiki <l10n-bot@translatewiki.net>
2024-01-01 12:36:12 +0000

    Deploying to gh-pages from @ wikimedia/wikipedia-preview@7e0e2bee6f33017536735c39b6a5c13ef0288dd3 🚀

--- main/storybook/assets/preview-aKoq0fQs.js
+++
@@ -1,8 +0,0 @@
-import{_ as t}from"./iframe-zadqQ9wj.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-Alm0RRFw.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4]),import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-aKoq0fQs.js.map
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-Alm0RRFw.js","./iframe-zadqQ9wj.js","./index-HZNADR7h.js","./index-aKxH7ZY0.js","./index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
