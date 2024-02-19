0210064b157f99bb59d98d624186f7a5b0c95522
translatewiki <l10n-bot@translatewiki.net>
2024-02-19 12:29:55 +0000

    Deploying to gh-pages from @ wikimedia/wikipedia-preview@df94981da0dee2236b766586bbcae560f15ed856 🚀

---
+++ main/storybook/assets/preview-rKETe2bR.js
@@ -0,0 +1,7 @@
+import{_ as t}from"./iframe-5wCAMBrd.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV--94MMtqc.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4]),import.meta.url);return new r}}};export{_ as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV--94MMtqc.js","./iframe-5wCAMBrd.js","./index-HZNADR7h.js","./index-ogXoivrg.js","./index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
