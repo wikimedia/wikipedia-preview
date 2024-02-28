867a9368e441a098d765914a7cb0b9a7b2d6295e
hueitan <hueitan@users.noreply.github.com>
2024-02-28 02:53:20 +0000

    Deploying to gh-pages from @ wikimedia/wikipedia-preview@4fcd041859252fb77ae3739599db9832456b50ed 🚀

--- T287095/storybook/assets/preview-4Ojy7zrl.js
+++
@@ -1,7 +0,0 @@
-import{_ as t}from"./iframe-B-f00bmD.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-W7JRmVy3.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4]),import.meta.url);return new r}}};export{_ as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-W7JRmVy3.js","./iframe-B-f00bmD.js","./index-HZNADR7h.js","./index-ogXoivrg.js","./index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
