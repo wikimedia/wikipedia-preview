bb1d8661abc2c0330db8208c8431b6ad201d2228
stephanebisson <stephane.c.bisson@gmail.com>
2024-03-18 13:13:21 +0000

    Deploying to gh-pages from @ wikimedia/wikipedia-preview@8a9e8ef750823d73a0338f4bf8170ba3daba1ab0 🚀

--- main/storybook/assets/preview-MXLirrqK.js
+++
@@ -1,7 +0,0 @@
-import{_ as t}from"./iframe-sdVJCUH4.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-eCkvmvv9.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4]),import.meta.url);return new r}}};export{_ as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-eCkvmvv9.js","./iframe-sdVJCUH4.js","./index-HZNADR7h.js","./index-ogXoivrg.js","./index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
