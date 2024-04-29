85066eedf3090373283aa4cdf659883a3db70202
translatewiki <l10n-bot@translatewiki.net>
2024-04-29 12:27:50 +0000

    Deploying to gh-pages from @ wikimedia/wikipedia-preview@7f71744460107840689238f6ee1b9456bc268437 🚀

--- main/storybook/assets/preview-kbVuRH4L.js
+++
@@ -1,7 +0,0 @@
-import{_ as t}from"./iframe-5A_W7lM7.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-alaRvJ9v.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4]),import.meta.url);return new r}}};export{_ as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-alaRvJ9v.js","./iframe-5A_W7lM7.js","./index-HZNADR7h.js","./index-ogXoivrg.js","./index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
