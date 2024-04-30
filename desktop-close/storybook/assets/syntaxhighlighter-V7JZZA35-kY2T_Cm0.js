5e5345f1f48371d07f182b0e4122d0a154b17651
stephanebisson <stephanebisson@users.noreply.github.com>
2024-04-30 15:30:28 +0000

    Deploying to gh-pages from @ wikimedia/wikipedia-preview@0941f29ffc075f4c7174937138b9b50e7574108e 🚀

---
+++ desktop-close/storybook/assets/syntaxhighlighter-V7JZZA35-kY2T_Cm0.js
@@ -0,0 +1,1 @@
+import{S as e,c as s,s as c}from"./DocsRenderer-NNNQARDV-IWAPIhHE.js";import"./iframe-iGeYJh0u.js";import"../sb-preview/runtime.js";import"./index-HZNADR7h.js";import"./index-ogXoivrg.js";import"./index-PPLHz8o0.js";export{e as SyntaxHighlighter,s as createCopyToClipboardFunction,c as default};
