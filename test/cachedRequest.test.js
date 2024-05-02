e3d0470c6e30bdb33e98e252d8ee7c1c249948f0
Sbisson <sbisson@wikimedia.org>
2024-05-02 13:20:39 -0400

    Fix test

--- test/cachedRequest.test.js
+++ test/cachedRequest.test.js
@@ -11,14 +11,14 @@ describe( 'cachedRequest', () => {
 	test( 'executes the transform function', () => {
 		return cachedRequest( 'url', ( d ) => `transformed ${ d }`, ( data ) => {
 			expect( data ).toBe( 'transformed data' )
-		}, requestMock( 'data' ) )
+		}, false, requestMock( 'data' ) )
 	} )
 
 	test( 'caches transformed output per URL', () => {
 		cachedRequest( 'url-2', () => 't-1', () => {
 			cachedRequest( 'url-2', () => 't-2', ( data ) => {
 				expect( data ).toBe( 't-1' )
-			}, requestMock( 'data-2' ) )
-		}, requestMock( 'data-1' ) )
+			}, false, requestMock( 'data-2' ) )
+		}, false, requestMock( 'data-1' ) )
 	} )
 } )
