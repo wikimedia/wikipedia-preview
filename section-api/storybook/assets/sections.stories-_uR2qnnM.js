77687d721af7a0aab21df1de68519a97d1f192e6
stephanebisson <stephanebisson@users.noreply.github.com>
2024-05-08 19:09:35 +0000

    Deploying to gh-pages from @ wikimedia/wikipedia-preview@de0d524cffb48c691e0a7e441caa4e4dd80c0b30 🚀

---
+++ section-api/storybook/assets/sections.stories-_uR2qnnM.js
@@ -0,0 +1,30 @@
+import{w as p}from"./index-AgDLAVYI.js";import"./fullscreen-u4xS-I_Y.js";import"./preview-fTb0o58F.js";const w={title:"Sections",argTypes:{lang:{name:"Language",control:"select",options:["en","fr","hi","ks","he","sv"]},title:{name:"Article Title",control:"text"}},args:{lang:"en",title:"Cat"},loaders:[async({args:{lang:e,title:i}})=>({articleText:await(await fetch(`https://${e}.wikipedia.org/api/rest_v1/page/mobile-html/${encodeURIComponent(i)}`)).text()})]},n=({lang:e,title:i},{loaded:{articleText:c}})=>{const a=new DOMParser().parseFromString(c,"text/html"),m=Array.from(a.querySelectorAll("section")),r=document.createElement("div");return m.forEach(d=>{let t=d.querySelector("h2, h3, h4, h5, h6");return t||(t=a.createElement("h2"),t.textContent="Summary"),t.setAttribute("data-wikipedia-preview",""),t.setAttribute("data-wp-title",i+"#"+t.id),t.setAttribute("data-wp-lang",e),t.style.maxWidth="fit-content",r.appendChild(t),t.outerHTML}),p.init({root:r,lang:e}),r};var o,l,s;n.parameters={...n.parameters,docs:{...(o=n.parameters)==null?void 0:o.docs,source:{originalSource:`({
+  lang,
+  title
+}, {
+  loaded: {
+    articleText
+  }
+}) => {
+  const doc = new DOMParser().parseFromString(articleText, 'text/html');
+  const sections = Array.from(doc.querySelectorAll('section'));
+  const container = document.createElement('div');
+  sections.forEach(sectionElement => {
+    let titleElement = sectionElement.querySelector('h2, h3, h4, h5, h6');
+    if (!titleElement) {
+      titleElement = doc.createElement('h2');
+      titleElement.textContent = 'Summary';
+    }
+    titleElement.setAttribute('data-wikipedia-preview', '');
+    titleElement.setAttribute('data-wp-title', title + '#' + titleElement.id);
+    titleElement.setAttribute('data-wp-lang', lang);
+    titleElement.style.maxWidth = 'fit-content';
+    container.appendChild(titleElement);
+    return titleElement.outerHTML;
+  });
+  wikipediaPreview.init({
+    root: container,
+    lang
+  });
+  return container;
+}`,...(s=(l=n.parameters)==null?void 0:l.docs)==null?void 0:s.source}}};const g=["Sections"];export{n as Sections,g as __namedExportsOrder,w as default};
