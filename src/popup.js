c17abd5fd0c03f2f9f1daa8a841ec98e236ca343
Huei Tan <huei90@gmail.com>
2024-05-08 11:02:28 +0800

    remove own compute position code

--- src/popup.js
+++ src/popup.js
@@ -3,62 +3,9 @@ import '../style/popup.less'
 
 let popup
 
-const computePopupPosition = (
-	targetRect,
-	popupWidth, popupHeight,
-	innerWidth, innerHeight
-) => {
-	const targetCenterX = targetRect.left + targetRect.width / 2
-	const targetCenterY = targetRect.top + targetRect.height / 2
-	const alignLeft = targetCenterX <= innerWidth / 2
-	const popupBelowTarget = targetCenterY <= innerHeight / 2
-
-	const left = alignLeft ?
-		targetRect.left :
-		targetRect.left + targetRect.width - popupWidth
-
-	const top = popupBelowTarget ?
-		targetRect.top + targetRect.height :
-		''
-
-	const bottom = popupBelowTarget ?
-		'' :
-		innerHeight - targetRect.top
-
-	return { left, top, bottom }
-}
-
 const withPx = ( value ) => {
 	return value ? ( value + 'px' ) : value
 }
-
-// Strangely, mouseenter often fires with the pointer slightly
-// outside any element rect. Making the rects bigger by a few pixel
-// ensures the pointer will be inside one of them.
-/*
-const expandRect = ( rect ) => {
-	const delta = 3
-	return {
-		left: rect.left - delta,
-		right: rect.right + delta,
-		top: rect.top - delta,
-		bottom: rect.bottom + delta
-	}
-}
-
-const getTargetRect = ( element, { x, y } ) => {
-	const rects = element.getClientRects()
-	for ( let i = 0; i < rects.length; i++ ) {
-		const rect = expandRect( rects[ i ] )
-		if ( x >= rect.left && x <= rect.right &&
-			y >= rect.top && y <= rect.bottom ) {
-			return rects[ i ]
-		}
-	}
-	// fallback for unit tests
-	return rects[ 0 ] || element.getBoundingClientRect()
-}
-*/
 
 const createPopup = ( container, win = window ) => {
 	if ( !popup ) {
@@ -137,4 +84,4 @@ const createPopup = ( container, win = window ) => {
 	return { show, hide, subscribe, element: popup }
 }
 
-export { createPopup, computePopupPosition }
+export { createPopup }
