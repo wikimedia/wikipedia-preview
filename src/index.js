40d8254dad4730d2c6a8ef2b6ba87ceeb7899bea
Sbisson <sbisson@wikimedia.org>
2024-05-14 09:01:55 -0400

    Fix debug print out

--- src/index.js
+++ src/index.js
@@ -236,13 +236,13 @@ function init( {
 		console.group( 'Wikipedia Preview [debug mode]' )
 		console.group( `Searching for "${ selector }" inside ${ root }, Total links found: ${ foundSelectorLinks.length }` )
 		foundSelectorLinks.forEach( ( link, index ) => {
-			console.log( index + 1, `${ link.text } -> ${ buildWikipediaUrl( link.lang, link.title, isTouch, false ) }` )
+			console.log( index + 1, `${ link.text } -> ${ decodeURI( buildWikipediaUrl( link.lang, link.title, isTouch, false ) ) }` )
 		} )
 		console.groupEnd()
 		if ( detectLinks ) {
 			console.group( `Searching for links to Wikipedia, Total links found: ${ foundDetectLinks.length }` )
 			foundDetectLinks.forEach( ( link, index ) => {
-				console.log( index + 1, `${ link.text } -> ${ buildWikipediaUrl( link.lang, link.title, isTouch, false ) }` )
+				console.log( index + 1, `${ link.text } -> ${ decodeURI( buildWikipediaUrl( link.lang, link.title, isTouch, false ) ) }` )
 			} )
 			console.groupEnd()
 		}
