280599ab2408d16bcac94b13e7f93c98692ce343
stephanebisson <stephanebisson@users.noreply.github.com>
2024-05-14 15:53:42 +0000

    Deploying to gh-pages from @ wikimedia/wikipedia-preview@c9f22c8fc06eca5fdb57fa16586c4c56d9fe1284 🚀

--- section-api/storybook/assets/preview-u6wwKg4g.js
+++
@@ -1,7 +0,0 @@
-import{_ as t}from"./iframe-kBHr3nfg.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-nqeh6tf3.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4]),import.meta.url);return new r}}};export{_ as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-nqeh6tf3.js","./iframe-kBHr3nfg.js","./index-HZNADR7h.js","./index-ogXoivrg.js","./index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
