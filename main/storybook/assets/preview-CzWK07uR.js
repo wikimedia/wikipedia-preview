a14fab1ed5d7e993b595c23f4685809c3ccbe1f3
stephanebisson <stephane.c.bisson@gmail.com>
2024-05-23 14:33:57 +0000

    Deploying to gh-pages from @ wikimedia/wikipedia-preview@a82469da57aae3496c7096102f6748635dc0796a 🚀

---
+++ main/storybook/assets/preview-CzWK07uR.js
@@ -0,0 +1,2 @@
+const __vite__fileDeps=["./DocsRenderer-NNNQARDV-DkbwqKdt.js","./iframe-DXZyxhZZ.js","./index-CoojMvHD.js","./index-D2DNihGs.js","./index-DrFu-skq.js"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
+import{_ as t}from"./iframe-DXZyxhZZ.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-DkbwqKdt.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4]),import.meta.url);return new r}}};export{_ as parameters};
