1fe3491029fc52caf162e7b39d27fc5ad3023d42
stephanebisson <stephanebisson@users.noreply.github.com>
2024-05-31 15:17:59 +0000

    Deploying to gh-pages from @ wikimedia/wikipedia-preview@cbde9ad4f2baa582f87a4f5de7a1242c25f2fa41 🚀

--- hide-scrollbars/storybook/assets/preview-DvHbc26B.js
+++
@@ -1,2 +0,0 @@
-const __vite__fileDeps=["./DocsRenderer-NNNQARDV-CDHgf6UJ.js","./iframe-54PMLRl4.js","./index-CoojMvHD.js","./index-D2DNihGs.js","./index-DrFu-skq.js"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
-import{_ as t}from"./iframe-54PMLRl4.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-CDHgf6UJ.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4]),import.meta.url);return new r}}};export{_ as parameters};
