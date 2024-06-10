a0c169d48ccdc92c743a46559f88f8df1a9dcbb3
Huei Tan <huei90@gmail.com>
2024-06-10 14:16:36 +0200

    fix test spec

--- cypress/e2e/mobile-gallery.cy.js
+++ cypress/e2e/mobile-gallery.cy.js
@@ -10,33 +10,6 @@ describe( 'Check the Gallery Pages in Mobile View', () => {
 		cy.navigateToMobileTestPage()
 	} )
 
-	it( 'Check the Gallery Pages Movement by Clicking on Image', () => {
-		// Open the Preview
-		preview.getPreviewSpan().first().click()
-		// Check for the Continue Reading exist
-		preview.getFooterContinueReading().then( ( res ) => {
-			if ( res.css( 'display' ) !== 'none' ) {
-				preview.getFooterContinueReading().click()
-			}
-		} )
-		const i = 0
-		// Check if the Images exist
-		preview.getBodyGalleryImages().its( 'length' ).then( () => {
-			// Open the Gallery
-			preview.getBodyGalleryImages().first().click()
-			// Check the Gallery Page
-			gallery.checkGalleryPage( i )
-			// Click on the Next Icon
-			gallery.getGalleryNextBtn().click()
-			// Click on the Previous Icon
-			gallery.getGalleryPrevBtn().click()
-			// Close the Gallery
-			gallery.getGalleryCloseBtn().click()
-			// Close the Preview
-			preview.getHeaderClosebtn().click()
-		} )
-	} )
-
 	it( 'Check the Gallery Pages Movement by Swiping', () => {
 		// Open the Preview
 		preview.getPreviewSpan().first().click()
@@ -52,7 +25,7 @@ describe( 'Check the Gallery Pages in Mobile View', () => {
 			// Open the Gallery
 			preview.getBodyGalleryImages().first().click()
 			// Check the Gallery Page
-			gallery.checkGalleryPage( i )
+			gallery.checkMobileGalleryPage( i )
 			// Swipe to go Next
 			gallery.getGalleryImage( i ).swipe( 'right', 'left' )
 			// Swipe to go Previous
@@ -75,12 +48,12 @@ describe( 'Check the Gallery Pages in Mobile View', () => {
 			// Open the Gallery
 			preview.getBodyGalleryImages().eq( 1 ).click()
 			// Check the Gallery Page
-			gallery.checkGalleryPage( i )
+			gallery.checkMobileGalleryPage( i )
 			// Check if the Caption Bar exist
-			gallery.getGalleryCaptionBar( i ).its( 'length' ).then( ( len ) => {
+			gallery.getGalleryCaption( i ).its( 'length' ).then( ( len ) => {
 				if ( len > 0 ) {
 					// Scroll the Caption Text
-					gallery.getGalleryCaptionBar( i ).click()
+					gallery.getGalleryCaption( i ).click()
 				}
 			} )
 		} )
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,8 +1,10 @@
 Cypress.Commands.add( 'navigateToTestPage', () => {
+	cy.viewport( 1220, 750 )
 	cy.visit( 'demo/articles/test.html' )
 } )
 
 Cypress.Commands.add( 'navigateToEnglishPage', () => {
+	cy.viewport( 1220, 750 )
 	cy.visit( 'demo/articles/english.html' )
 } )
 
