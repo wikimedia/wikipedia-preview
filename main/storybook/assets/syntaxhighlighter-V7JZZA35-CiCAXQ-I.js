0191c363badae325ca786303f811b8548ca550e1
translatewiki <l10n-bot@translatewiki.net>
2024-06-10 12:25:17 +0000

    Deploying to gh-pages from @ wikimedia/wikipedia-preview@27c762e00a4d45315a8836d702106e7f92b4116c 🚀

---
+++ main/storybook/assets/syntaxhighlighter-V7JZZA35-CiCAXQ-I.js
@@ -0,0 +1,1 @@
+import{S as e,c as s,s as c}from"./DocsRenderer-NNNQARDV-Ddboamy4.js";import"./iframe-B99j0pBX.js";import"../sb-preview/runtime.js";import"./index-CoojMvHD.js";import"./index-D2DNihGs.js";import"./index-DrFu-skq.js";export{e as SyntaxHighlighter,s as createCopyToClipboardFunction,c as default};
