c9065cdd1ebdb4e89f346607f2d259a35cde2e2c
translatewiki <l10n-bot@translatewiki.net>
2024-06-13 12:29:52 +0000

    Deploying to gh-pages from @ wikimedia/wikipedia-preview@66b6b44aab17b7f89b752033a1cc56601ade6de5 🚀

--- main/storybook/assets/preview-ldI6awRE.js
+++
@@ -1,2 +0,0 @@
-const __vite__fileDeps=["./DocsRenderer-NNNQARDV-TIz0BiYg.js","./iframe-CuToAb86.js","./index-CoojMvHD.js","./index-D2DNihGs.js","./index-DrFu-skq.js"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
-import{_ as t}from"./iframe-CuToAb86.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-TIz0BiYg.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4]),import.meta.url);return new r}}};export{_ as parameters};
