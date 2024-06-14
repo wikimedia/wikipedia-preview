ec00f0991dc6fb60e81ffd8770c3a5275bbb3fbd
stephanebisson <stephanebisson@users.noreply.github.com>
2024-06-14 14:53:09 +0000

    Deploying to gh-pages from @ wikimedia/wikipedia-preview@741cf5da7db7c4f6cf5599e8dbbfffa3af8ef650 🚀

---
+++ desktop-touch/storybook/assets/preview-20Vmdg6z.js
@@ -0,0 +1,2 @@
+const __vite__fileDeps=["./DocsRenderer-NNNQARDV-DUzCMHhL.js","./iframe-DLUKJ6f4.js","./index-CoojMvHD.js","./index-D2DNihGs.js","./index-DrFu-skq.js"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
+import{_ as t}from"./iframe-DLUKJ6f4.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-DUzCMHhL.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4]),import.meta.url);return new r}}};export{_ as parameters};
