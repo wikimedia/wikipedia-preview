9bf13775be4c1bc4cc921b84bb80b5db6d48ec72
medied <medied@users.noreply.github.com>
2024-06-19 20:42:16 +0000

    Deploying to gh-pages from @ wikimedia/wikipedia-preview@7bc18f6dd166a17fb8f511c3204479c62b423f28 🚀

---
+++ T360753-gallery-images/storybook/assets/preview-B_Edf4Tw.js
@@ -0,0 +1,2 @@
+const __vite__fileDeps=["./DocsRenderer-NNNQARDV-HBY6f6Og.js","./iframe-IyE0Q-s8.js","./index-CoojMvHD.js","./index-D2DNihGs.js","./index-DrFu-skq.js"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
+import{_ as t}from"./iframe-IyE0Q-s8.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-HBY6f6Og.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4]),import.meta.url);return new r}}};export{_ as parameters};
