fe2846c0d3cf0da7b57c4481e44e7f4a675d6177
hueitan <hueitan@users.noreply.github.com>
2024-06-21 12:34:16 +0000

    Deploying to gh-pages from @ wikimedia/wikipedia-preview@4cea3b25106ee7e8c14a8fef538cebe3673c867f 🚀

--- T364134/storybook/assets/preview-k22pKuwk.js
+++
@@ -1,2 +0,0 @@
-const __vite__fileDeps=["./DocsRenderer-NNNQARDV-DAEmt6oB.js","./iframe-Ba6zOlL4.js","./index-CoojMvHD.js","./index-D2DNihGs.js","./index-DrFu-skq.js"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
-import{_ as t}from"./iframe-Ba6zOlL4.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-DAEmt6oB.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4]),import.meta.url);return new r}}};export{_ as parameters};
