f009c5b24662d2f513c8f48b09c08d62437b7fa1
translatewiki <l10n-bot@translatewiki.net>
2024-07-02 09:27:54 +0000

    Deploying to gh-pages from @ wikimedia/wikipedia-preview@5cbec913c0c5cd497480cadc6fc1016403459d09 🚀

--- main/storybook/assets/WithTooltip-V3YHNWJZ-zAqEZTvY.js
+++
@@ -1,1 +0,0 @@
-import{W,W as T,a as e}from"./DocsRenderer-NNNQARDV-x8MJb6Tb.js";import"./iframe-C3FNReRi.js";import"../sb-preview/runtime.js";import"./index-CoojMvHD.js";import"./index-D2DNihGs.js";import"./index-DrFu-skq.js";export{W as WithToolTipState,T as WithTooltip,e as WithTooltipPure};
--- main/storybook/assets/preview-QuuFDo-q.js
+++
@@ -1,2 +0,0 @@
-const __vite__fileDeps=["./DocsRenderer-NNNQARDV-x8MJb6Tb.js","./iframe-C3FNReRi.js","./index-CoojMvHD.js","./index-D2DNihGs.js","./index-DrFu-skq.js"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
-import{_ as t}from"./iframe-C3FNReRi.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-x8MJb6Tb.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4]),import.meta.url);return new r}}};export{_ as parameters};
