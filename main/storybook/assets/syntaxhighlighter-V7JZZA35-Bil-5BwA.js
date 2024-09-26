4b32c23a927cd036cbe9087051ea396150d1e417
translatewiki <l10n-bot@translatewiki.net>
2024-09-26 12:33:54 +0000

    Deploying to gh-pages from @ wikimedia/wikipedia-preview@65fea1930ab78a111651a9bb74cc5762e116c89f 🚀

--- main/storybook/assets/syntaxhighlighter-V7JZZA35-Bil-5BwA.js
+++
@@ -1,1 +0,0 @@
-import{S as e,c as s,s as c}from"./DocsRenderer-NNNQARDV-A8p8qXHT.js";import"./iframe-D3BAPnWf.js";import"../sb-preview/runtime.js";import"./index-CoojMvHD.js";import"./index-D2DNihGs.js";import"./index-DrFu-skq.js";export{e as SyntaxHighlighter,s as createCopyToClipboardFunction,c as default};
