cc339001db1ae04ec522dd7974bba0aff9ef971d
translatewiki <l10n-bot@translatewiki.net>
2025-05-22 12:37:32 +0000

    Deploying to gh-pages from @ wikimedia/wikipedia-preview@ab66cf0a05f48bae235ad6a52ef9e5e5ce604df1 🚀

--- main/storybook/assets/preview-BpC67ktF.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./DocsRenderer-NNNQARDV-D1XmziBt.js","./iframe-DBWv4fxP.js","./doctrine-h9SWtg8o.js","./index-D2DNihGs.js","./index-DrFu-skq.js"])))=>i.map(i=>d[i]);
-import{_ as t}from"./iframe-DBWv4fxP.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-D1XmziBt.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4]),import.meta.url);return new r}}};export{_ as parameters};
